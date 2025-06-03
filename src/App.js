acffc310ec9f96d068eed21185bf7c32bac52212
Jonathan Zhang <yufengzhang483@gmail.com>
2025-06-03 17:51:08 -0400

    use state

--- src/App.js
+++ src/App.js
@@ -2,36 +2,49 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("mario");
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "Mario's Birthday Bash", id: 1 },
     { title: "Browser's live stream", id: 2 },
     { title: "Race on moo moo farm", id: 3 },
   ]);
 
+  console.log(showEvents);
+
   const handleClick = (id) => {
     console.log(id);
-    setEvents(
-      events.filter((event) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
         return id !== event.id;
-      })
-    );
+      });
+    });
   };
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}> change name </button>
-      {events.map((events, index) => (
-        <div key={events.id}>
-          <h2>
-            {index} - {events.title}
-            <button onClick={() => handleClick(events.id)}>
-              Deletet Event
-            </button>
-          </h2>
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
         </div>
-      ))}
+      )}
+
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+
+      {showEvents &&
+        events.map((events, index) => (
+          <div key={events.id}>
+            <h2>
+              {index} - {events.title}
+              <button onClick={() => handleClick(events.id)}>
+                Deletet Event
+              </button>
+            </h2>
+          </div>
+        ))}
     </div>
   );
 }
---
+++ src/components/title.js
@@ -0,0 +1,9 @@
+export default function Title() {
+  return (
+    <div>
+      <h1 className="title"> Mushroom Kingdom Events</h1>
+      <br />
+      <h2 className="subtitle"> All the latesst events in Mushroom Kingdom</h2>
+    </div>
+  );
+}
