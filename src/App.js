518f39722f1f4cdeab12c4280fd21c36418a53ca
Jonathan Zhang <yufengzhang483@gmail.com>
2025-06-03 09:28:57 -0400

    use prev state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("mario");
+  const [events, setEvents] = useState([
+    { title: "Mario's Birthday Bash", id: 1 },
+    { title: "Browser's live stream", id: 2 },
+    { title: "Race on moo moo farm", id: 3 },
+  ]);
+
+  const handleClick = (id) => {
+    console.log(id);
+    setEvents(
+      events.filter((event) => {
+        return id !== event.id;
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}> change name </button>
+      {events.map((events, index) => (
+        <div key={events.id}>
+          <h2>
+            {index} - {events.title}
+            <button onClick={() => handleClick(events.id)}>
+              Deletet Event
+            </button>
+          </h2>
+        </div>
+      ))}
     </div>
   );
 }
